52a05733f14ccd8c90f56d9f952d7e5f25e159e9
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2023-05-12 15:36:30 +0400

    Update

--- BMVox.cpp
+++ BMVox.cpp
@@ -5,19 +5,31 @@
 #include <Windows.h>
 #pragma comment(lib, "Winmm.lib")
 using namespace std;
-int main()
+int main(int argc, char* argv[])
 {
-    string str;
-    getline(cin, str);
-    string s;
-    stringstream ss(str);
-    vector<string> v;
-    while (getline(ss, s, ' ')) 
+    if (argc <= 1)
     {
-        v.push_back(s);
+        string str;
+        cout << "Command: ";
+        getline(cin, str);
+        string s;
+        stringstream ss(str);
+        vector<string> v;
+        while (getline(ss, s, ' '))
+        {
+            v.push_back(s);
+        }
+        for (size_t i = 0; i < v.size(); i++)
+        {
+            PlaySoundA(("VOX\\" + v[i] + ".wav").c_str(), nullptr, SND_SYNC | SND_NODEFAULT);
+        }
     }
-    for (size_t i = 0; i < v.size(); i++) 
+    if (argc > 1)
     {
-        PlaySoundA(("VOX\\" + v[i] + ".wav").c_str(), nullptr, SND_SYNC);
+        for (size_t i = 1; i < argc; i++)
+        {
+            string s = argv[i];
+            PlaySoundA(("VOX\\" + s + ".wav").c_str(), nullptr, SND_SYNC | SND_NODEFAULT);
+        }
     }
 }
