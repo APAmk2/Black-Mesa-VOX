3f5208816fb909e5b0e3b782f2d9771067183bf7
APAmk2 <107351397+APAmk2@users.noreply.github.com>
2023-05-12 11:01:24 +0400

    Sources upload

---
+++ BMVox.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <Windows.h>
+#pragma comment(lib, "Winmm.lib")
+using namespace std;
+int main()
+{
+    string str;
+    getline(cin, str);
+    string s;
+    stringstream ss(str);
+    vector<string> v;
+    while (getline(ss, s, ' ')) 
+    {
+        v.push_back(s);
+    }
+    for (size_t i = 0; i < v.size(); i++) 
+    {
+        PlaySoundA(("VOX\\" + v[i] + ".wav").c_str(), nullptr, SND_SYNC);
+    }
+}
